a6ea0f076373648a7da2afd1ac3aec9226e6deab
chaichai-fe <2768505574@qq.com>
2022-12-05 15:27:40 +0800

    pro init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Edit from './components/Edit.vue'
+
+</script>
+
+<template>
+  <div class="app">
+    <el-table :data="[{
+      id: 1,
+      name: 'jack',
+      place: 'none'
+    }]">
+      <el-table-column label="ID" prop="id"></el-table-column>
+      <el-table-column label="姓名" prop="name" width="150"></el-table-column>
+      <el-table-column label="籍贯" prop="place"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template #default>
+          <el-button type="primary" link>编辑</el-button>
+          <el-button type="danger" link>删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+  <Edit />
+</template>
+
+<style scoped>
+.app {
+  width: 980px;
+  margin: 100px auto 0;
+}
+</style>
